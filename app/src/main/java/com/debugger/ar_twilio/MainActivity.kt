3512ce7ddfe186dea46e7de00ac0d0504a0d3adf
HaiderSaleem <hsaleem72@gmail.com>
2020-02-02 20:28:25 +0500

    First Commit
    Layouts and other res

---
+++ app/src/main/java/com/debugger/ar_twilio/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.debugger.ar_twilio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
